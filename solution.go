e59c2ec432bbed91c01ac3906d310849d6678265
kingmetok <kingmetok@gmail.com>
2022-02-17 21:27:33 +0200

    Homework-1: Implemented calculator of tha squares

--- solution.go
+++ solution.go
@@ -1,5 +1,9 @@
 package square
 
+import (
+	"math"
+)
+
 // Define custom int type to hold sides number and update CalcSquare signature by replacing #yourTypeNameHere#
 
 // Define constants to represent 0, 3 and 4 sides.  Test uses mnemos: SidesTriangle(==3), SidesSquare(==4), SidesCircle(==0)
@@ -8,5 +12,21 @@ package square
 // CalcSquare(10.0, SidesSquare)
 // CalcSquare(10.0, SidesCircle)
 
-func CalcSquare(sideLen float64, sidesNum #yourTypeNameHere#) float64 {
+type sides int
+
+const (
+	SidesCircle   sides = 0
+	SidesTriangle sides = 3
+	SidesSquare   sides = 4
+)
+
+func CalcSquare(sideLen float64, sidesNum sides) float64 {
+	switch sidesNum {
+	case SidesTriangle:
+		return math.Sqrt(sideLen*sideLen*3) / 4
+	case SidesSquare, SidesCircle:
+		return sideLen * sideLen
+	default:
+		panic("Incorrect sides num")
+	}
 }
